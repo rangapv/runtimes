419cdfd9e03070640bae29053d5fa06eb30de7c8
rangapv <rangapv@yahoo.com>
2021-06-12 15:58:54 +0000

    Added support for SUSE distro

--- runcontainerD.sh
+++ runcontainerD.sh
@@ -9,7 +9,15 @@ else
 	mac=$(sw_vers | grep Mac)
 fi
 
-d1=$(cat /etc/*-release | grep ID= | grep debian)
+  u1=$(cat /etc/*-release | grep ID= | grep ubuntu)
+  f1=$(cat /etc/*-release | grep ID= | grep fedora)
+  r1=$(cat /etc/*-release | grep ID= | grep rhel)
+  a1=$(cat /etc/*-release | grep ID= | grep amzn)
+  c1=$(cat /etc/*-release | grep ID= | grep centos)
+  s1=$(cat /etc/*-release | grep ID= | grep sles)
+  d1=$(cat /etc/*-release | grep ID= | grep debian)
+  fc1=$(cat /etc/*-release | grep ID= | grep flatcar)
+
 
 if [ -z "$mac" ]
 then
@@ -52,10 +60,28 @@ then
     else	    
     echo "containerd is running at $check1"
     fi
+elif [ ! -z "$s1" ]
+then
+    ji=$(cat /etc/*-release | grep '^ID=' |awk '{split($0,a,"\"");print a[2]}')
+    ki="${ji,,}"
+    if [ $ki = "sles" ]
+    then
+    echo " it is SUSE"
+    # sudo zypper install -y gcc make openssl-devel libffi-devel zlib-devel wget lsb-release
+    count=1
+    fi
+    check1=`which containerd`
+    check2=$(echo "$?")
+    if [ $check2 -ne 0 ]
+    then
+     sudo zypper addrepo https://download.opensuse.org/repositories/Virtualization:containers/SLE_15_SP2/Virtualization:containers.repo
+     sudo zypper refresh
+     sudo zypper install -y containerd
+    else	    
+    echo "containerd is running at $check1"
+    fi
 
 fi
-
-
 
 
 fi #end of Mac check
