50bdec0e3ada62113c1d263299aebc526c5d87b8
rangapv <rangapv@yahoo.com>
2021-06-13 12:38:42 +0000

    Added source to do linux TYPE CHECKING and also containerd for Ubuntu

--- runcontainerD.sh
+++ runcontainerD.sh
@@ -1,45 +1,14 @@
 #!/usr/bin/bash
 set -E
-li=$(uname -s)
-
-if [ $(echo $li | grep Linux) ]
-then
-	mac=""
-else
-	mac=$(sw_vers | grep Mac)
-fi
-
-  u1=$(cat /etc/*-release | grep ID= | grep ubuntu)
-  f1=$(cat /etc/*-release | grep ID= | grep fedora)
-  r1=$(cat /etc/*-release | grep ID= | grep rhel)
-  a1=$(cat /etc/*-release | grep ID= | grep amzn)
-  c1=$(cat /etc/*-release | grep ID= | grep centos)
-  s1=$(cat /etc/*-release | grep ID= | grep sles)
-  d1=$(cat /etc/*-release | grep ID= | grep debian)
-  fc1=$(cat /etc/*-release | grep ID= | grep flatcar)
-
-
-if [ -z "$mac" ]
-then
-        
-	echo "It is a Linux Box" 
-
-else
-	echo "It is a Mac"
-
-fi
+source <(curl -s https://raw.githubusercontent.com/rangapv/bash-source/main/s1.sh)
 
 if [ -z "$mac" ]
 then
 
 if [ ! -z "$d1" ]
 then
-    ji=$(cat /etc/*-release | grep ^ID= | grep -v "\"" | awk '{split($0,a,"=");print a[2]}') 
-    ki="${ji,,}"
     if [ $ki = "debian" ]
     then
-     echo "It is Debian"
-    fi
     check1=`which containerd`
     check2=$(echo "$?")
     if [ $check2 -ne 0 ]
@@ -60,13 +29,11 @@ then
     else	    
     echo "containerd is running at $check1"
     fi
+    fi
 elif [ ! -z "$s1" ]
 then
-    ji=$(cat /etc/*-release | grep '^ID=' |awk '{split($0,a,"\"");print a[2]}')
-    ki="${ji,,}"
     if [ $ki = "sles" ]
     then
-    echo " it is SUSE"
     # sudo zypper install -y gcc make openssl-devel libffi-devel zlib-devel wget lsb-release
     count=1
     fi
@@ -80,6 +47,27 @@ then
     else	    
     echo "containerd is running at $check1"
     fi
+elif [ ! -z "$u1" ]
+then
+   if [ $ki = "ubuntu" ]
+   then
+   check1=`which containerd`
+   check2=$(echo "$?")
+   if [ $check2 -ne 0 ]
+   then
+   sudo $cm1 -y update
+   sudo $cm1 install -y gnupg lsb-release apt-transport-https ca-certificates
+   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+   echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null 
+   sudo $cm1 update
+   sudo $cm1 install containerd.io
+   sudo mkdir -p /etc/containerd
+   containerd config default | sudo tee /etc/containerd/config.toml
+   sudo systemctl restart containerd
+   else
+    echo "containerd is running at $check1"
+   fi
+   fi
 
 fi
 
