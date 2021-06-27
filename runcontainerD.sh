b722f5e49f3f4eba4eb13bb0cce589cd1a5d733d
rangapv <rangapv@yahoo.com>
2021-06-27 06:12:47 +0000

    Added IPV4 FORWARD

--- runcontainerD.sh
+++ runcontainerD.sh
@@ -3,12 +3,11 @@ set -E
 source <(curl -s https://raw.githubusercontent.com/rangapv/bash-source/main/s1.sh)
 
 configtoml (){
-
 file1="/etc/containerd/config.toml"
 line2="\[plugins\.\"io\.containerd\.grpc\.v1\.cri\"\.containerd\.runtimes\.runc\.options\]"
 line3="\ \ \ \ \ \ \ \ \ \ \ \ SystemdCgroup = true"
 sudo sed -i "/$line2/a     $line3" $file1
-
+echo "1" | sudo tee /proc/sys/net/ipv4/ip_forward
 }
 
 if [ -z "$mac" ]
